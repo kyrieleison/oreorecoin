06072b00349e4147dcc766c97144b6e11840725b
Yuka Sato <yuka.sato@gaiax.com>
2018-02-24 15:49:26 +0900

    add blacklist

--- contracts/OreOreCoin.sol
+++ contracts/OreOreCoin.sol
@@ -6,8 +6,19 @@ contract OreOreCoin {
   uint8 public decimals;
   uint256 public totalSupply;
   mapping (address => uint256) public balanceOf;
+  mapping (address => int8) public blackList;
+  address public owner;
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
 
   event Transfer(address indexed from, address indexed to, uint256 value);
+  event Blacklisted(address indexed target);
+  event DeleteFromBlacklist(address indexed target);
+  event RejectedPaymentToBlacklistedAddr(address indexed from, address indexed to, uint256 value);
+  event RejectedPaymentFromBlacklistedAddr(address indexed from, address indexed to, uint256 value);
 
   function OreOreCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) public {
     balanceOf[msg.sender] = _supply;
@@ -15,15 +26,32 @@ contract OreOreCoin {
     symbol = _symbol;
     decimals = _decimals;
     totalSupply = _supply;
+    owner = msg.sender;
+  }
+
+  function blacklisting(address _addr) public onlyOwner() {
+    blackList[_addr] = 1;
+    Blacklisted(_addr);
+  }
+
+  function deleteFromBlacklist(address _addr) public onlyOwner() {
+    blackList[_addr] = -1;
+    DeleteFromBlacklist(_addr);
   }
 
   function transfer(address _to, uint256 _value) public {
     require(balanceOf[msg.sender] >= _value);
     require(balanceOf[_to] + _value >= balanceOf[_to]);
 
-    balanceOf[msg.sender] -= _value;
-    balanceOf[_to] += _value;
+    if (blackList[msg.sender] > 0) {
+      RejectedPaymentFromBlacklistedAddr(msg.sender, _to, _value);
+    } else if (blackList[_to] > 0) {
+      RejectedPaymentToBlacklistedAddr(msg.sender, _to, _value);
+    } else {
+      balanceOf[msg.sender] -= _value;
+      balanceOf[_to] += _value;
 
-    Transfer(msg.sender, _to, _value);
+      Transfer(msg.sender, _to, _value);
+    }
   }
 }
